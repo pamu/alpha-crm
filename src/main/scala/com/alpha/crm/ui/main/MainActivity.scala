53f86b1ffb22752676f9549d627b78f1d3a1b655
pamu <p_nagarjuna@students.iitmandi.ac.in>
2016-02-17 12:34:13 +0530

    toolbar

--- src/main/scala/com/alpha/crm/ui/main/MainActivity.scala
+++ src/main/scala/com/alpha/crm/ui/main/MainActivity.scala
@@ -2,9 +2,13 @@ package com.alpha.crm.ui.main
 
 import android.app._
 import android.os.Bundle
+import com.alpha.crm.R
 
 class MainActivity extends Activity {
+
   override def onCreate(savedInstanceState: Bundle): Unit = {
-    
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.layout_main_acitivity)
   }
+
 }
